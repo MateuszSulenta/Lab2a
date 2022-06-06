2740bcc45522d467829f87a3fda0d63e54db7429
MateuszSulenta <100996793+MateuszSulenta@users.noreply.github.com>
2022-06-06 20:10:57 +0200

    Realizacja zadania 2. Wybór pliku xml w interfejsie GUI.

--- Lab2a/src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ Lab2a/src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -3,9 +3,12 @@ package pl.lublin.wsei.java.cwiczenia;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-
+import javafx.stage.FileChooser;
+import java.io.File;
 
 public class HelloController {
+
+
     @FXML
     private Label welcomeText;
 
@@ -16,9 +19,21 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
     public Label lbFile;
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*xml)", "*.xml");
 
-
+    @FXML
+    public void initialize(){
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
 
     public void btnOpenFIleAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            lbFile.setText(file.getAbsolutePath());
+        }
+        else {
+            lbFile.setText("Proszę wczytać plik...");
+        }
     }
 }
